a74e2eb4e8a25a6af2969144beb86954ecc2d3ad
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-11-26 23:02:01 +0000

    Toggle sound sprite

--- src/Scenes/Gameplay/Components/SoundButton.cs
+++ src/Scenes/Gameplay/Components/SoundButton.cs
@@ -3,8 +3,38 @@ using Minesweeper.Scenes.Gameplay;
 
 public class SoundButton : TextureButton
 {
+    [Export] private Texture NormalSoundOn;
+    [Export] private Texture HoverSoundOn;
+    [Export] private Texture PressedSoundOn;
+    [Export] private Texture NormalSoundOff;
+    [Export] private Texture HoverSoundOff;
+    [Export] private Texture PressedSoundOff;
+
+    public override void _Ready()
+    {
+        SetSprite();
+    }
+
     private void _on_SoundButton_pressed()
     {
         Settings.ToggleSound();
+        SetSprite();
+    }
+
+    private void SetSprite()
+    {
+        var settings = Settings.GetInstance();
+        if (settings.SoundEnabled)
+        {
+            TextureNormal = NormalSoundOn;
+            TextureHover = HoverSoundOn;
+            TexturePressed = PressedSoundOn;
+        }
+        else
+        {
+            TextureNormal = NormalSoundOff;
+            TextureHover = HoverSoundOff;
+            TexturePressed = PressedSoundOff;
+        }
     }
 }
